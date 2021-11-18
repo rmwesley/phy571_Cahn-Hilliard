1f5eba17e51bf5715fecb53902befef1bd8e3dcf
Wesley Rodrigues Machado <wesley.machado.2018@polytechnique.org>
2021-11-18 02:02:22 +0100

    Spectral method implemented Also, some sample visuals are provided in visualization.ipynb

---
+++ design/spectral.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class Spectral:
+    """Spectral stepping method for solving PDEs"""
+    def __init__(self, Lx, Ly, Nx, Ny, eps, initialization='random'):
+
+        """Initialize the spatial and spectral grids"""
+        self.Lx, self.Ly = Lx, Ly
+        self.Nx, self.Ny = Nx, Ny
+        self.x, self.dx = np.linspace(0, Lx, Nx, retstep=True)
+        self.y, self.dy = np.linspace(0, Ly, Ny, retstep=True)
+        self.kx = 2 * np.pi * np.fft.fftfreq(Nx, d=self.dx).reshape(-1, 1)
+        self.ky = 2 * np.pi * np.fft.fftfreq(Ny, d=self.dy).reshape(1, -1)
+        
+        self.k2 = self.kx**2 * np.ones([1, Ny]) + np.ones([Nx, 1]) * self.ky**2
+        if initialization == 'trivial_x':
+            solution = lambda x: np.tanh(x/(np.sqrt(2)*eps))
+            self.u = solution(np.linspace(-.5,.5, Nx)).reshape(-1, 1) * np.ones([1, Ny])
+        elif initialization == 'trivial_y':
+            solution = lambda y: np.tanh(y/(np.sqrt(2)*eps))
+            self.u = np.ones([Nx, 1]) * solution(np.linspace(-.5,.5, Ny)).reshape(-1, 1)
+        elif initialization == 'separated_x':
+            self.u = np.concatenate((np.ones([(Nx+1)//2, Ny]), -np.ones([Nx//2, Ny])))
+        elif initialization == 'separated_y':
+            self.u = np.concatenate((np.ones([Nx, (Ny+1)//2]), -np.ones([Nx, Ny//2])), axis=1)
+        else:
+            self.u = np.random.uniform(-1, 1, [Nx, Ny])
+        self.f = lambda t: t**3 - t
+        self.eps = eps
+
+    def advance(self, dt, Nt):
+        u_hat = np.fft.fft2(self.u)
+        #print(u_hat.shape, self.k2.shape, self.f(self.u).shape, np.fft.fft2(self.f(self.u)).shape)
+        for i in range(Nt):
+            u_hat = ((u_hat - dt * self.k2 * np.fft.fft2(self.f(self.u)))/
+                (1 + dt * self.eps**2 * self.k2**2))
+            #u_hat = ((u_hat - dt * self.k2 * np.fft.fft2(self.f(self.u)))/
+            #    (1 + 2*dt*self.k2  + dt * self.eps**2 * self.k2**2))
+        self.u = np.fft.ifft2(u_hat).real
+
+    def step(self, dt):
+        """Make a step dt forward in time"""
+        self.advance(dt, 1)
--- readme.md
+++ readme.md
@@ -5,23 +5,22 @@ pandoc readme.md -o readme.pdf)
 # Phase separation of the components of a binary fluid (AC)
 
 Instructions/advices:
-- The main paper to start with for the possible algorithms leading to a
+
+> The main paper to start with for the possible algorithms leading to a
 practical scheme for solving the Cahn-Hilliard equation is Lee_CMS2014.pdf
 
-- Jamet.pdf gives details about the physics of phase separation for a
+> Jamet.pdf gives details about the physics of phase separation for a
 binary fluid and the derivation of the Cahn-Hilliard equation you will
 solve numerically.
 
-- DJ.Eyre_1998.pdf is useful if you are interested in the way to
+> DJ.Eyre_1998.pdf is useful if you are interested in the way to
 perform an operator splitting that will stabilize some of the schemes
 you may write naively.
 
-- TCGPhysica1995.pdf will possibly give you some ideas of diagnostics
+> TCGPhysica1995.pdf will possibly give you some ideas of diagnostics
 that you can implement, once your solver will be running, to
 characterize the patterns obtained in phase separation.
 
-To compile this file as a pdf run the following command:
-
-`pandoc readme.md -o readme.pdf`
+Run `pandoc readme.md -o readme.pdf` to compile this markdown file as a pdf
 
 **Project done in collaboration by students Rayen Mahjoub, Gaspard Daumas and Wesley Rodrigues Machado. Supervised by professor Arnaud Couairon.**
