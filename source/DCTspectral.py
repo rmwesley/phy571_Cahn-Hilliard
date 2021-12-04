6cef1ef75f7b72d81caac84165ea648b790be05a
Wesley Rodrigues Machado <wesley.machado.2018@polytechnique.org>
2021-12-04 17:45:02 +0100

    Implemented the DCT spectral method. Also added an histogram to visualization.ipynb and updated it. Added some paragraphs on the density of free energy, or the "energy gradient" term more specifically, on report.tex as well.

--- source/DCTspectral.py
+++ source/DCTspectral.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.fft import dctn, idctn, fftfreq
 
 class Spectral:
     """Spectral stepping method for solving the Cahn-Hilliard equation"""
@@ -10,10 +11,8 @@ class Spectral:
         self.Nx, self.Ny = Nx, Ny
         self.x, self.dx = np.linspace(0, Lx, Nx, retstep=True)
         self.y, self.dy = np.linspace(0, Ly, Ny, retstep=True)
-        #self.kx = np.linspace(0, (Nx-1)/Lx, Nx).reshape(-1, 1)
-        #self.ky = np.linspace(0, (Ny-1)/Ly, Ny).reshape(1, -1)
-        self.kx = 2 * np.pi * np.fft.fftfreq(Nx, d=self.dx).reshape(-1, 1)
-        self.ky = 2 * np.pi * np.fft.fftfreq(Ny, d=self.dy).reshape(1, -1)
+        self.kx = 2 * np.pi * np.linspace(0, (Nx-1)/Lx, Nx).reshape(-1, 1)
+        self.ky = 2 * np.pi * np.linspace(0, (Ny-1)/Ly, Ny).reshape(1, -1)
 
         self.k2 = self.kx**2 * np.ones([1, Ny]) + np.ones([Nx, 1]) * self.ky**2
         if initialization == 'trivial_x':
@@ -40,28 +39,28 @@ class Spectral:
         if step==None: self.step = self.step_method5
         self.eps = eps
 
-    def advance(self, u_hat, Nt):
+    def advance(self, u_hat, dt, Nt):
         """Advance a spectral grid temporally. Does not update any os the object's attributes."""
         for i in range(Nt):
             u_hat = self.step(u_hat, dt)
+        return u_hat
 
     def evolve(self, reps, dt, Nt):
         """Evolves the system temporally, updating self.u 'reps' times."""
         """To calculate the updated value, we evolve the wavelength spectral decomposition (Fourier transform) of self.u with the advance procedure. We then go back to the space domain through an inverse Fourier transform."""
         for i in range(reps):
-            u_hat = np.fft.fft2(self.u)
-            for i in range(Nt):
-                u_hat = self.step(u_hat, dt)
-            self.u = np.fft.ifft2(u_hat).real
+            u_hat = dctn(self.u, norm='ortho')
+            u_hat = self.advance(u_hat, dt, Nt)
+            self.u = idctn(u_hat, norm='ortho')
     
     def step_method2(self, u_hat, dt):
         """Spectral version of semi-implicit Euler's scheme"""
         return ((u_hat -
-            dt * self.k2 * np.fft.fft2(self.func(self.u))) /
+            dt * self.k2 * dctn(self.func(self.u), norm='ortho')) /
             (1 + dt * self.eps**2 * self.k2**2))
     def step_method5(self, u_hat, dt):
         """Spectral version of linearly stabilized splitting scheme"""
         return ((u_hat -
-            dt * self.k2 * (np.fft.fft2(self.u**3)) +
+            dt * self.k2 * (dctn(self.u**3, norm='ortho')) +
             3*dt * self.k2 * u_hat ) /
             (1 + dt * (self.eps**2 * self.k2**2 + 2*self.k2)))
